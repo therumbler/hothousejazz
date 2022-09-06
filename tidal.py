b575188ddad3d0672c6ea4c6c8d58b8dc405dad5
Benjamin Rumble <brumble@tucows.com>
2022-09-06 17:19:34 -0400

    @lru_cache not @cache

--- tidal.py
+++ tidal.py
@@ -1,4 +1,4 @@
-from functools import cache
+from functools import lru_cache
 import json
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
@@ -6,7 +6,7 @@ from urllib.parse import quote
 
 
 class Tidal:
-    @cache
+    @lru_cache
     def search_artist(self, artist):
         url = f"https://api.tidalhifi.com/v1/search?types=artists&token=CzET4vdadNUFQ5JU&countryCode=US&query={quote(artist)}"
         print("searching %s" % artist)
